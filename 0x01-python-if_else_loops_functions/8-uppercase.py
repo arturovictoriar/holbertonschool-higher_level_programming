0673202f6c415f909b430b850c39279c99068c27
arvicrin <1231@holbertonschool.com>
2019-12-10 21:34:29 +0000

    Convert all the lower char into upper char, fix number of print

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    newline = ""
     for i in range(len(str)):
         if i == len(str) - 1:
-            if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-                print("{:c}".format(ord(str[i]) - 32))
-            else:
-                print("{}".format(str[i]))
-        elif ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-            print("{:c}".format(ord(str[i]) - 32), end="")
+            newline = "\n"
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            print("{:c}{}".format(ord(str[i]) - 32, newline), end="")
         else:
-            print("{}".format(str[i]), end="")
+            print("{}{}".format(str[i], newline), end="")
