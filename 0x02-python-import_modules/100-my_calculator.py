1e28f8d90cb1854b8a5a53ba0e075cb87871247e
arvicrin <1231@holbertonschool.com>
2019-12-12 23:55:16 +0000

    Import all functions from the file calculator_1.py and handles basics operations, fix operators and conditional

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,28 +6,22 @@ import sys
 def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        sign = ['+', '-', '*', '/']
-        signok = 0
-        for i in sign:
-            print("{}".format(i))
-            if sys.argv[2] == i:
-                signok = 1
-                break
-        if signok == 0:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-        else:
-            a = sys.argv[1]
-            b = sys.argv[3]
-            if sys.argv[2] == sign[0]:
-                print("{:s} + {:s} = {:d}".format(a, b, add(int(a), int(b))))
-            elif sys.argv[2] == sign[1]:
-                print("{:s} - {:s} = {:d}".format(a, b, sub(int(a), int(b))))
-            elif sys.argv[2] == sign[2]:
-                print("{:s} * {:s} = {:d}".format(a, b, mul(int(a), int(b))))
-            elif sys.argv[2] == sign[3]:
-                print("{:s} / {:s} = {:d}".format(a, b, div(int(a), int(b))))
+        sys.exit(1)
+    
+    operator = ['+', '-', '*', '/']
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == operator[0]:
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sys.argv[2] == operator[1]:
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == operator[2]:
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == operator[3]:
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 if __name__ == "__main__":
     main()
