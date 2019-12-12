ae346a211c90d2ffc87773a913fb351ab567b46d
arvicrin <1231@holbertonschool.com>
2019-12-12 17:02:59 +0000

    Print the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def main():
+    sumita = 0
+    for i in range(1, len(sys.argv)):
+        sumita += int(sys.argv[i])
+    print("{}".format(sumita))
+if __name__ == "__main__":
+    main()
