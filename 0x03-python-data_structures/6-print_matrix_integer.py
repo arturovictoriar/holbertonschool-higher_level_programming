742960d7e82088ae89e3dad3add6bd65e3de5dd1
arvicrin <1231@holbertonschool.com>
2019-12-17 18:47:52 +0000

    Print a matrix of integers, fix print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) != 1 and len(matrix) != 0 and matrix:
+    if (len(matrix) != 1 and len(matrix) != 0):
         for j in range(len(matrix)):
             for i in range(len(matrix[j])):
                 if i != len(matrix[j]) - 1:
-                    print("{:d}".format(matrix[j][i]), end=" ")
+                    print("{:d} ".format(matrix[j][i]), end="")
                 else:
                     print("{:d}".format(matrix[j][i]))
     else:
-        print()
+        print("")
