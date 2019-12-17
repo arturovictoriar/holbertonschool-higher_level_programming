c22add86527466be394675c81edfbd0564463368
arvicrin <1231@holbertonschool.com>
2019-12-17 18:24:37 +0000

    Print a matrix of integers, fix condtion

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) != 1 and len(matrix) != 0:
+    if len(matrix) != 1 and len(matrix) != 0 and matrix:
         for j in range(len(matrix)):
             for i in range(len(matrix[j])):
                 if i != len(matrix[j]) - 1:
