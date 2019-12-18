21e42caf895236587aceefd9350636077d208fe2
arvicrin <1231@holbertonschool.com>
2019-12-18 04:18:41 +0000

    Print some basic info about Python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,12 +12,12 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t allo = (((PyListObject *)(p))->allocated);
 	PyObject *ele;
 
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", allo);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allo);
 
 	for (i = 0; i < size; i++)
 	{
 		ele = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", size, (char *)(ele->ob_type)->tp_name);
+		printf("Element %d: %s\n", size, (char *)(ele->ob_type)->tp_name);
 	}
 }
