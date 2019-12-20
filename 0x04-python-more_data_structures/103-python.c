cb0f7559573418ea87b577a64208d748c90ae7d9
arvicrin <1231@holbertonschool.com>
2019-12-20 03:27:42 +0000

    No working

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+  * print_python_list - Print python list info
+  * @p: modulo object
+  * Return: Nothing
+  */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i, size = (((PyVarObject *)(p))->ob_size);
+	Py_ssize_t allo = (((PyListObject *)(p))->allocated);
+	PyObject *ele;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allo);
+
+	for (i = 0; i < size; i++)
+	{
+		ele = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, (char *)(ele->ob_type)->tp_name);
+	}
+}
+
+/**
+  * print_python_bytes - Print python list info
+  * @p: modulo object
+  * Return: Nothing
+  */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t i, size = (((PyVarObject *)(p))->ob_size);
+	Py_ssize_t allo = (((PyListObject *)(p))->allocated);
+	PyObject *ele;
+
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allo);
+
+	for (i = 0; i < size; i++)
+	{
+		ele = PyList_GetItem(p, i);
+	printf("Element %zd: %s\n", i, (char *)(ele->ob_type)->tp_name);
+	}
+}
