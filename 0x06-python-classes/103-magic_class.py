47680b3ff0827e783aa768dc1998e12c6358eca7
arvicrin <1231@holbertonschool.com>
2020-01-10 01:30:21 +0000

    Python function from Python bytecode., fix and condition

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,12 +4,12 @@ import math
 class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
-    def area(self):
+    def area():
         return self.__radius ** 2 * math.pi
 
-    def circumference(self):
+    def circumference():
         return 2 * math.pi * self.__radius
