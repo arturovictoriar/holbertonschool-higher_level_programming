63dd09322faf358da5dcfa1961ecf8a1f10d5b9b
arvicrin <1231@holbertonschool.com>
2020-01-21 19:34:33 -0500

    Function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object., fix json

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -10,4 +10,4 @@ import json
 def class_to_json(obj):
     """Function that writes an Object to \
     a text file, using a JSON representation."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
