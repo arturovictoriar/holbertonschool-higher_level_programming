a6e99a25326cc82edaf6eaa801fa3112871b8d17
arvicrin <1231@holbertonschool.com>
2020-01-26 19:32:42 +0000

    Class Base, private class attribute __nb_objects = 0, class constructor: def __init__(self, id=None).

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+    base.py module
+"""
+
 
 class Base:
+    """ Base class"""
+
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """Inicialitation method"""
         if id is not None:
             self.id = id
         else:
-            __nb_objects = __nb_objects + 1
-            self.id = id
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
