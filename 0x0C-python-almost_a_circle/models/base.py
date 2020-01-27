4f04cc4eb3605b9493291cfced377403a422f916
arvicrin <1231@holbertonschool.com>
2020-01-27 03:46:55 +0000

    Update the class Base by adding the class method def save_to_file(cls, list_objs): that writes the JSON string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,39 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        l = []
+        try:
+            f = open(str(type(list_objs[0]).__name__) + ".json")
+            f.close()
+        except:
+            with open(str(type(list_objs[0]).__name__) + ".json", "w") as f:
+                f.write(cls.to_json_string([]))
+
+        if len(list_objs) == 0:
+            with open(str(type(list_objs[0]).__name__) + ".json", "r") as f:
+                l = json.loads(f.read())
+
+            with open(str(type(list_objs[0]).__name__) + ".json", "w") as f:
+                f.write(cls.to_json_string(l))
+
+        else:
+            with open(str(type(list_objs[0]).__name__) + ".json", "r+") as f:
+                z = f.read()
+                if len(z) == 0:
+                    f.write(cls.to_json_string([]))
+
+            with open(str(type(list_objs[0]).__name__) + ".json", "r") as f:
+                l = json.loads(f.read())
+
+            with open(str(type(list_objs[0]).__name__) + ".json", "w") as f:
+                for el in list_objs:
+                    l.append(el.to_dictionary())
+
+                f.write(cls.to_json_string(l))
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries"""
