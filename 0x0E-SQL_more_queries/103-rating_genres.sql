34d94de57d9eb269162cc804c8f7ede80239f42b
arvicrin <1231@holbertonschool.com>
2020-03-04 05:49:29 +0000

    lists all genres in the database hbtn_0d_tvshows_rate by their rating, fix # lines

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,9 +2,7 @@
 -- Holberton School
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id 
-INNER JOIN tv_show_ratings
-ON tv_show_genres.show_id = tv_show_ratings.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY SUM(tv_show_ratings.rate) DESC;
