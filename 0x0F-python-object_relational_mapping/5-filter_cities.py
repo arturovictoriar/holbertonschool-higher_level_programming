a084efce8e439950e025e5c9d368bfb42fba80a2
arvicrin <1231@holbertonschool.com>
2020-03-28 19:51:25 +0000

    All cities by state, delete INNER JOIN

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,10 +15,9 @@ def main():
                          db=argv[3])
     c = db.cursor()
     command = """SELECT cities.name
-                 FROM cities
-                 INNER JOIN states
-                 ON cities.state_id = states.id
+                 FROM cities, states
                  WHERE BINARY states.name = %s
+                 AND cities.state_id = states.id
                  ORDER BY cities.id ASC"""
     numrows = c.execute(command, (argv[4],))
     states = c.fetchall()
