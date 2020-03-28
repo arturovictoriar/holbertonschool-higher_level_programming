85b9949ec5937f88c31fa8cd497dfc9398f6b4b6
arvicrin <1231@holbertonschool.com>
2020-03-28 19:36:59 +0000

    All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Get the cities of 1 state of a database
+"""
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """Only executes when is not imported"""
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         port=3306,
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    command = """SELECT cities.name
+                 FROM cities, states
+                 WHERE BINARY states.name = %s
+                 AND cities.state_id = states.id
+                 ORDER BY states.id ASC"""
+    numrows = c.execute(command, (argv[4],))
+    states = c.fetchall()
+    for i, idstate in enumerate(states):
+        if (i != numrows - 1):
+            print("{}, ".format(idstate[0]), end='')
+        else:
+            print("{}".format(idstate[0]))
+
+if __name__ == "__main__":
+    main()
