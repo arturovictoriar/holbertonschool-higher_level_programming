04c547f7bc008c0be4e41209788f485266db53a2
arvicrin <1231@holbertonschool.com>
2020-04-09 18:20:52 +0000

    Fix condition if len list == 1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,10 +6,12 @@
 
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
-    if list_of_integers:
-        peak = list_of_integers[0]
-    else:
+    if len(list_of_integers) == 0:
         return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        peak = list_of_integers[0]
 
     for i, num in enumerate(list_of_integers):
         if num >= list_of_integers[len(list_of_integers) - 1 - i]:
