ccc178622b66968b5bd1b0516ed1dba9e15c28da
arvicrin <1231@holbertonschool.com>
2020-05-05 17:46:33 +0000

    Fix a conts

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-/*
-  searche and print the second biggest integer in the list of arguments.
-*/
+
 const args = process.argv;
 const len = process.argv.length;
 if (len <= 3) {
   console.log(0);
 } else {
   args.sort();
-  console.log(args[len - 2]);
+  const secondBig = args[len - 2];
+  console.log(secondBig);
 }
