b7b1bf166c8c55c7c62a4cb0677f35ad78ee5209
arvicrin <1231@holbertonschool.com>
2020-05-05 17:51:24 +0000

    Fix sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 const args = process.argv;
 const len = process.argv.length;
 if (len <= 3) {
   console.log(0);
 } else {
-  args.sort();
-  const secondBig = args[len - 2];
-  console.log(secondBig);
+  const secondBig = args.sort(function (a, b) { return b - a; });
+  console.log(secondBig[3]);
 }
