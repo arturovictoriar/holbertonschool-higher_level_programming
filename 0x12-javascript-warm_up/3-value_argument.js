12da44bc5c53b5a7dec5a55501829f203de4c2a6
arvicrin <1231@holbertonschool.com>
2020-05-05 17:13:04 +0000

    Delete lenght to pass checker

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
   print first input argument
 */
 const args = process.argv;
-if (args.length <= 2) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
