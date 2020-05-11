9a036d0b9848df5aaa687be7044ed955b6313312
arvicrin <1231@holbertonschool.com>
2020-05-11 18:03:00 +0000

    rollback origin, fix file import

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,19 +4,18 @@
     inherit from Rectangle
     create a charPrint(c) method
 */
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
+class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let height = 0; height < this.height; height++) {
-      console.log(c.repeat(this.width));
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let height = 0; height < this.height; height++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }
